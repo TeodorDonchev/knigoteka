e31dcef7594ab2755445eedbf9a82cdedace6884
TeodorDonchev <teodorgdo@gmail.com>
2020-07-28 05:08:12 +0300

    feat: created aside component, updated css and made header more responsive

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './index.module.css';
 
-const PageTitle = ({text}) => {
+const PageTitle = ({ text }) => {
     return (
         <h1 className={styles['page-title']}>{text}</h1>
     );
--- src/pages/all-books-page/index.js
+++ src/pages/all-books-page/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import Book from '../../components/book';
 import PageLayout from '../../components/page-layout';
 import PageTitle from '../../components/title';
-import MainButton from '../../components/main-button';
+import Aside from '../../components/aside';
 import styles from './index.module.css';
 
 const AllBooksPage = () => {
     return (
         <PageLayout>
+            <Aside className={styles.aside}/>
             <PageTitle text="All Books"/>
-            <div className={styles.container}>
-                <MainButton text="+ Add Book"/>
-            </div>
             <Book page="all"/>
         </PageLayout>
     );
