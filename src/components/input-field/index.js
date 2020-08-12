06177d2fb769febff31bddb8b073465377755415
TeodorDonchev <teodorgdo@gmail.com>
2020-08-12 12:28:48 +0300

    feat: added cloudinary api for image uploads

--- src/components/input-field/index.js
+++ src/components/input-field/index.js
@@ -3,10 +3,18 @@ import styles from './index.module.css';
 
 
 const InputField = ({ type, name, value, placeholder, onChange }) => {
+    if (type === 'file') {
+        return (
+            <label htmlFor="file-upload" className={styles['cover-upload']}>
+                {placeholder}  
+                <input id="file-upload" type={type} name={name} onChange={onChange}/>
+            </label>
+        )
+    }
     return (
         <div className={styles['input-container']}>
             <input className={styles['input-field']}
-                type={type} 
+                type={type}
                 name={name}
                 placeholder={placeholder}
                 value={value}
